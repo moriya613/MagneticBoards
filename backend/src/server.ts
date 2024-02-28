a64c0e63d1a4bde4d068ff9b6b9817e14e3ec079
moriya613 <120042087+moriya613@users.noreply.github.com>
2024-02-28 02:09:54 +0200

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
+import path from 'path';
 import express from "express";
 import cors from "cors";
 import itemRouter from "./routers/items.router";
