4c3fc253c8e04a47836a6c324f6cb43688d41783
moriya13 <moriya.raz@gmail.com>
2024-03-20 12:41:41 -0700

    cros add origin

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cors({
     credentials:true,
-    origin:["http://localhost:4200", "http://localhost:1000", "http://localhost:5000"]
+    origin:["http://localhost:4200", "http://localhost:1000", "http://localhost:5000", "https://magneticboards.onrender.com"]
 }));
 
 app.use("/api/items", itemsRouter);
