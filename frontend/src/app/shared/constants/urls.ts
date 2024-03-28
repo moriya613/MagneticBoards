b7c212236437245a2978bd78b9257ea73c03cc0c
moriya13 <moriya.raz@gmail.com>
2024-03-27 23:12:19 -0700

    urls file

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -1,7 +1,7 @@
 import { environment } from "../../../environments/environment.prod";
 
-//const BASE_URL =  environment.production? '' : 'http://localhost:5000';
-const BASE_URL =  'http://localhost:5000';
+const BASE_URL =  environment.production? '' : 'http://localhost:5000';
+//const BASE_URL =  'http://localhost:5000';
 
 
 
@@ -18,13 +18,14 @@ export const USER_LOGIN_URL = BASE_URL  + '/api/users/login';
 export const USER_REGISTER_URL = BASE_URL + '/api/users/register';
 
 export const USER_REMOVE_URL = BASE_URL + '/api/users/remove';
+export const USER_GET_ADMIN_OF_SCHOOL_CODE = BASE_URL + '/api/users/getAdmonSchoolcode'
 
 export const ORDERS_URL = BASE_URL + '/api/orders';
 export const ORDER_CREATE_URL = ORDERS_URL + '/create';
 export const ORDER_CHANGE_STATUS_TO_APPROVE = ORDERS_URL + '/changeStatusToApprove'
 export const ORDER_NEW_FOR_CURRENT_SCHOOL_CODE = ORDERS_URL + '/newOrdersForCurrentSchoolCode';
 export const ORDER_FOR_CURRENT_USER_URL = ORDERS_URL + '/OrdersForCurrentUser';
-
+export const ORDER_ADMINS_ORDERS_URL= ORDERS_URL + '/getAllAdminsOrders';
 export const ORDER_NEW_FOR_CURRENT_USER_URL = ORDERS_URL + '/newOrderForCurrentUser';
 
 export const ORDER_PAY_URL = ORDERS_URL + '/pay';
