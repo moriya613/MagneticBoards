f71887eb432a984d8c8f4aed44745ba38f659b61
moriya13 <moriya.raz@gmail.com>
2024-04-09 03:25:32 -0700

    proportions

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,83 +1,7 @@
 import { Item } from "./models/item.model";
 
 export const sample_items: Item[] = [
-    {
-      id:'1',
-      name: 'לוח ה100',
-      price: 10,
-      favorite: false,
-      uses: 4,
-      imageUrl: 'assets/100Board.png',
-      tags: ['חשבון'],
-      width: 8,
-      length: 10,
-      schoolCharacter: 'ממד'
-    },
-    {
-      id:'2',
-      name: 'אלול',
-      price: 20,
-      favorite: true,
-      uses: 7,
-      imageUrl: 'assets/Elul.png',
-      tags: ['יהדות'],
-      width: 17,
-      length: 15,
-      schoolCharacter: 'ממד'
-
-    },
-    {
-      id:'3',
-      name: 'אותיות',
-      price: 5,
-      favorite: false,
-      uses: 3,
-      imageUrl: 'assets/letters.png',
-      tags: ['תורה'],
-      width: 10,
-      length: 10,
-      schoolCharacter: 'ממד'
-
-    },
-    {
-      id:'4',
-      name: 'מה בקלמר',
-      price: 2,
-      favorite: true,
-      uses: 3,
-      imageUrl: 'assets/MaBakalmar.png',
-      tags: ['כללי'],
-      width: 12,
-      length: 12,
-      schoolCharacter: 'ממד'
-
-    },
-    {
-      id:'5',
-      name: 'כותרת חשבון',
-      price: 11,
-      favorite: false,
-      uses: 0,
-      imageUrl: 'assets/MathTitle.png',
-      tags: ['חשבון'],
-      width: 10,
-      length: 5 ,   
-      schoolCharacter: 'ממד'
-    },
-
-    {
-      id:'6',
-      name: 'תוצרי כתיבה',
-      price: 9,
-      favorite: false,
-      uses: 4,
-      imageUrl: 'assets/WriteProdct.png',
-      tags: ['עברית'],
-      width: 7,
-      length: 7,
-      schoolCharacter: 'ממד'
-
-    },
+   
     {
       id:'7',
       name: 'יש לי יום הולדת',
@@ -86,9 +10,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/Birthday.jpg',
       tags: ['כללי'],
-      width: 5,
-      length: 7,
-      schoolCharacter: 'ממד'
+      width: 50,
+      length: 70,
+      schoolCharacter: ''
 
     },
 
@@ -100,9 +24,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/EngLetters.jpg',
       tags: ['אנגלית'],
-      width: 7,
-      length: 5,
-      schoolCharacter: 'ממד'
+      width: 30,
+      length: 40,
+      schoolCharacter: ''
 
     },
 
@@ -114,9 +38,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/GirlBaloon.jpg',
       tags: ['כללי'],
-      width: 7,
-      length: 5,
-      schoolCharacter: 'ממד'
+      width: 30,
+      length: 30,
+      schoolCharacter: 'ממלכתי'
 
     },
 
@@ -128,9 +52,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/Hebrew2.jpg',
       tags: ['עברית'],
-      width: 7,
-      length: 5,
-      schoolCharacter: 'ממד'
+      width: 40,
+      length: 25,
+      schoolCharacter: ''
 
     },
     {
@@ -141,9 +65,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/longU.jpg',
       tags: ['אנגלית'],
-      width: 4,
-      length: 7,
-      schoolCharacter: 'חרדי'
+      width: 20,
+      length: 30,
+      schoolCharacter: ''
 
     },
     {
@@ -154,9 +78,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/NumbersHouse.jpg',
       tags: ['חשבון'],
-      width: 5,
-      length: 5,
-      schoolCharacter: 'חרדי'
+      width: 50,
+      length: 50,
+      schoolCharacter: ''
 
     },
 
@@ -168,9 +92,9 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/PlusMath.jpg',
       tags: ['חשבון'],
-      width: 5,
-      length: 5,
-      schoolCharacter: 'ממלכתי'
+      width: 50,
+      length: 50,
+      schoolCharacter: ''
 
     },
 
@@ -182,9 +106,543 @@ export const sample_items: Item[] = [
       uses: 4,
       imageUrl: 'assets/Time.jpg',
       tags: ['עברית'],
-      width: 5,
-      length: 5,
-      schoolCharacter: 'ממלכתי'
+      width: 40,
+      length: 26,
+      schoolCharacter: ''
+
+    },
+    {
+      id:'15-1',
+      name: 'כדור צהוב',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור צהוב.jpg',
+      tags: ['כללי'],
+      width: 13, // רוחב
+      length: 13,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'15-2',
+      name: 'כדור צהוב',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור צהוב.jpg',
+      tags: ['כללי'],
+      width: 10, // רוחב
+      length: 10,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'15-3',
+      name: 'כדור צהוב',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור צהוב.jpg',
+      tags: ['כללי'],
+      width: 6, // רוחב
+      length: 6,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'15-4',
+      name: 'כדור צהוב',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור צהוב.jpg',
+      tags: ['כללי'],
+      width: 4, // רוחב
+      length: 4,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'16-1',
+      name: 'כדור כתום',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור כתום.jpg',
+      tags: ['כללי'],
+      width: 4, // רוחב
+      length: 4,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'16-2',
+      name: 'כדור כתום',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור כתום.jpg',
+      tags: ['כללי'],
+      width: 6, // רוחב
+      length: 6,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'16-3',
+      name: 'כדור כתום',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור כתום.jpg',
+      tags: ['כללי'],
+      width: 10, // רוחב
+      length: 10,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'16-4',
+      name: 'כדור כתום',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור כתום.jpg',
+      tags: ['כללי'],
+      width: 13, // רוחב
+      length: 13,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'17-1',
+      name: 'כדור ורוד',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ורוד.jpg',
+      tags: ['כללי'],
+      width: 13, // רוחב
+      length: 13,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'17-2',
+      name: 'כדור ורוד',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ורוד.jpg',
+      tags: ['כללי'],
+      width: 4, // רוחב
+      length: 4,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'17-3',
+      name: 'כדור ורוד',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ורוד.jpg',
+      tags: ['כללי'],
+      width: 6, // רוחב
+      length: 6,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'17-4',
+      name: 'כדור ורוד',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ורוד.jpg',
+      tags: ['כללי'],
+      width: 10, // רוחב
+      length: 10,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'18-1',
+      name: 'כדור סגול',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור סגול.jpg',
+      tags: ['כללי'],
+      width: 13, // רוחב
+      length: 13,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'18-2',
+      name: 'כדור סגול',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור סגול.jpg',
+      tags: ['כללי'],
+      width: 4, // רוחב
+      length: 4,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'18-3',
+      name: 'כדור סגול',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור סגול.jpg',
+      tags: ['כללי'],
+      width: 6, // רוחב
+      length: 6,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'18-4',
+      name: 'כדור סגול',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור סגול.jpg',
+      tags: ['כללי'],
+      width: 10, // רוחב
+      length: 10,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'19-1',
+      name: 'כדור ירוק',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ירוק.jpg',
+      tags: ['כללי'],
+      width: 13, // רוחב
+      length: 13,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'19-2',
+      name: 'כדור ירוק',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ירוק.jpg',
+      tags: ['כללי'],
+      width: 4, // רוחב
+      length: 4,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'19-3',
+      name: 'כדור ירוק',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ירוק.jpg',
+      tags: ['כללי'],
+      width: 6, // רוחב
+      length: 6,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'19-4',
+      name: 'כדור ירוק',
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כדור ירוק.jpg',
+      tags: ['כללי'],
+      width: 10, // רוחב
+      length: 10,  // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'20', //
+      name: 'בלון צהוב',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/בלון צהוב.jpg', //
+      tags: ['כללי'],
+      width: 17,// רוחב
+      length:  29, // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'21', //
+      name:'בלון לב צהוב',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/בלון לב צהוב.jpg',
+      tags: ['כללי'],
+      width: 18,// רוחב
+      length: 27 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'22-1', //
+      name:'1וילון אדום',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון אדום1.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'22-2', //
+      name:'2וילון אדום',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון אדום2.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'23-1', //
+      name:'1וילון כחול',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון כחול1.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'23-2', //
+      name:'2וילון כחול',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון כחול2.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'24-1', //
+      name:'1וילון לבן',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון לבן1.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'24-2', //
+      name:'2וילון לבן',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/וילון לבן2.jpg',
+      tags: ['כללי'],
+      width: 34,// רוחב
+      length: 117 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'25', //
+      name:'בלון כתום',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/בלון כתום.jpg',
+      tags: ['כללי'],
+      width: 56,// רוחב
+      length: 70 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'26', //
+      name:'בלון כחול',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/בלון כחול.jpg',
+      tags: ['כללי'],
+      width: 56,// רוחב
+      length: 70 , // אורך
+      schoolCharacter: ''
+
+    },
+    {
+      id:'27', //
+      name:'ילדה קוראת',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/ילדה קוראת.jpg',
+      tags: ['כללי'],
+      width: 20,// רוחב
+      length: 30 , // אורך
+      schoolCharacter: 'ממד'
+
+    },
+    {
+      id:'28', //
+      name:'ילדה על ספר',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/ילדה על ספר.jpg',
+      tags: ['כללי'],
+      width: 26,// רוחב
+      length: 52 , // אורך
+      schoolCharacter: 'ממד'
+
+    },
+    {
+      id:'29', //
+      name:'ילד קורא',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/ילד קורא.jpg',
+      tags: ['כללי'],
+      width: 18,// רוחב
+      length: 32 , // אורך
+      schoolCharacter: 'חרדי'
+
+    },
+   
+    {
+      id:'30', //
+      name:'2ילד קורא',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/ילד קורא2.jpg',
+      tags: ['כללי'],
+      width: 37,// רוחב
+      length: 42 , // אורך
+      schoolCharacter: 'ממד'
+
+    },
+    {
+      id:'31', //
+      name:'ילדה עם ספרים',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/ילדה עם ספרים.jpg',
+      tags: ['כללי'],
+      width: 40,// רוחב
+      length: 50 , // אורך
+      schoolCharacter: 'ממד'
+
+    },
+    { 
+      id:'32', //
+      name:'דגל צבעוני',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/דגל צבעוני.jpg',
+      tags: ['כללי'],
+      width: 93,// רוחב
+      length: 18 , // אורך
+      schoolCharacter: ''
+
+    },
+    { 
+      id:'33', //
+      name:'דגלים',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/דגלים.jpg',
+      tags: ['כללי'],
+      width: 93,// רוחב
+      length: 18 , // אורך
+      schoolCharacter: ''
+
+    },
+    { 
+      id:'34', //
+      name:'2דגלים',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/דגלים2.jpg',
+      tags: ['כללי'],
+      width: 93,// רוחב
+      length: 18 , // אורך
+      schoolCharacter: ''
+
+    },
+    { 
+      id:'35', //
+      name:'כותרת וילון אדום',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כותרת וילון אדום.jpg',
+      tags: ['כללי'],
+       width: 114,// רוחב
+      length: 10 , // אורך
+      schoolCharacter: ''
+
+    },
+    { 
+      id:'36', //
+      name:'כותרת וילון צהוב',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כותרת וילון צהוב.jpg',
+      tags: ['כללי'],
+      width: 114,// רוחב
+      length: 10 , // אורך
+      schoolCharacter: ''
+
+    },
+    { 
+      id:'37', //
+      name:'כותרת וילון תכלת',  //
+      price: 9,
+      favorite: false,
+      uses: 4,
+      imageUrl: 'assets/כותרת וילון תכלת.jpg',
+      tags: ['כללי'],
+      width: 114,// רוחב
+      length: 10 , // אורך
+      schoolCharacter: ''
 
     },
   ];
