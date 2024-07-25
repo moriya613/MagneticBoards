33d390f76367a29abb8e2bfc3b4283cdbdb72c45
moriya13 <moriya.raz@gmail.com>
2024-07-25 01:30:42 -0700

    push

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -3,7 +3,6 @@ import { environment } from "../../../environments/environment.prod";
 //const BASE_URL =  environment.production? '' : 'http://localhost:5000';
 const BASE_URL =  'http://localhost:5000';
 
-
 export const ITEMS_URL = BASE_URL + '/api/items';
 export const ITEMS_TAGS_URL = ITEMS_URL + '/tags';
 export const ITEMS_BY_SEARCH_URL = ITEMS_URL + '/search/';
